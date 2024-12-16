4b46aaa01334b34fcb29ec0b26b91df0570be55c
lucasdeinani <125698870+lucasdeinani@users.noreply.github.com>
2024-12-15 23:01:08 -0400

    Exercícios

---
+++ curso_python/aula158_professor/pessoas.py
@@ -0,0 +1,26 @@
+class Pessoa:
+    def __init__(self, nome: str, idade: int) -> None:
+        self.nome = nome
+        self.idade = idade
+
+    @property
+    def nome(self) -> str:
+        return self._nome
+
+    @nome.setter
+    def nome(self, nome: str) -> None:
+        self._nome = nome
+
+    @property
+    def idade(self) -> int:
+        return self._idade
+
+    @idade.setter
+    def idade(self, idade: int) -> None:
+        self._idade = idade
+
+
+class Cliente(Pessoa):
+    def __init__(self, nome: str, idade: int) -> None:
+        super().__init__(nome, idade)
+        self.conta = None
