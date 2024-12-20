9449dd78ed28e11930efe2420674f538de70bdda
lucasdeinani <125698870+lucasdeinani@users.noreply.github.com>
2024-12-19 23:28:35 -0400

    Aulas

--- curso_python/aula158_professor/banco.py
+++ curso_python/aula158_professor/banco.py
@@ -15,23 +15,37 @@ class Banco:
 
     def _checa_agencia(self, agencia):
         if agencia.agencia in self.agencias:
+            print('_checa_agencia', True)
             return True
+        print('_checa_agencia', False)
         return False
 
     def _checa_cliente(self, cliente):
         if cliente in self.clientes:
+            print('_checa_cliente', True)
             return True
+        print('_checa_cliente', False)
         return False
 
     def _checa_conta(self, conta):
         if conta in self.contas:
+            print('_checa_conta', True)
             return True
+        print('_checa_conta', False)
         return False
 
-    def autenticar(self, cliente, conta):
+    def _checa_se_conta_e_do_cliente(self, cliente, conta):
+        if conta is cliente.conta:
+            print('_checa_se_conta_e_do_cliente', True)
+            return True
+        print('_checa_se_conta_e_do_cliente', False)
+        return False
+
+    def autenticar(self, cliente: pessoas.Pessoa, conta: contas.Conta):
         return self._checa_agencia(conta) and \
             self._checa_cliente(cliente) and \
-            self._checa_conta(conta)
+            self._checa_conta(conta) and \
+            self._checa_se_conta_e_do_cliente(cliente, conta)
 
     def __repr__(self):
         class_name = type(self).__name__
@@ -48,4 +62,10 @@ if __name__ == '__main__':
     c2.conta = cp1
     banco = Banco()
     banco.clientes.extend([c1, c2])
-    print(banco)
+    banco.contas.extend([cc1, cp1])
+    banco.agencias.extend([111, 222])
+
+    if banco.autenticar(c1, cc1):
+        cc1.depositar(10)
+        c1.conta.depositar(100)
+        print(c1.conta)
---
+++ curso_python/aula159_IV.py
@@ -0,0 +1,25 @@
+# dataclasses - O que são dataclasses?
+# O módulo dataclasses fornece um decorador e funções para criar métodos como
+# __init__(), __repr__(), __eq__() (entre outros) em classes definidas pelo
+# usuário.
+# Em resumo: dataclasses são syntax sugar para criar classes normais.
+# Foi descrito na PEP 557 e adicionado na versão 3.7 do Python.
+# doc: https://docs.python.org/3/library/dataclasses.html
+from dataclasses import dataclass, field  # , fields
+
+
+@dataclass
+class Pessoa:
+    nome: str = field(
+        default='MISSING', repr=False
+    )
+    sobrenome: str = 'Not send'
+    idade: int = 0
+    enderecos: list[str] = field(default_factory=list)
+
+
+if __name__ == '__main__':
+    p1 = Pessoa()
+    # p1 = Pessoa('Luiz', 'Otávio')
+    # print(fields(p1))
+    print(p1)
