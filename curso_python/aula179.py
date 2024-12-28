56cc775541023cb47aa81d6828652b3659c89182
lucasdeinani <125698870+lucasdeinani@users.noreply.github.com>
2024-12-28 12:33:15 -0400

    Aulas

--- curso_python/aula179.py
+++ curso_python/aula179.py
@@ -1,7 +1,26 @@
 # csv.reader e csv.DictReader
 # csv.reader lê o CSV em formato de lista
 # csv.DictReader lê o CSV em formato de dicionário
+import csv
 from pathlib import Path
 
 CAMINHO_CSV = Path(__file__).parent / 'aula179.csv'
-print(CAMINHO_CSV)
+
+# with open(CAMINHO_CSV, 'r', encoding='UTF-8') as arquivo:
+#     leitor = csv.reader(arquivo)
+#     # next(leitor)
+
+#     # print(next(leitor))
+
+#     for linha in leitor:
+#         print(linha[0])
+
+
+with open(CAMINHO_CSV, 'r', encoding='UTF-8') as arquivo:
+    leitor = csv.DictReader(arquivo)
+    # next(leitor)
+
+    # print(next(leitor))
+
+    for linha in leitor:
+        print(linha['Nome'], linha['Idade'], linha['Endereço'])
