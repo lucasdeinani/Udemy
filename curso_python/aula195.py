f27edf00b0ddd35c908d70bfed68a8edbd70d2cb
lucasdeinani <125698870+lucasdeinani@users.noreply.github.com>
2024-12-31 20:00:47 -0400

    AUlasss

---
+++ curso_python/aula195.py
@@ -0,0 +1,16 @@
+# Jupyter Notebook - Instalação e teste
+# Comumente utilizado por Cientistas de Dados
+# No terminal para abrir o Jupyter deve utilizar
+# jupyter notebook
+
+# Já vem instaldo como padrão as 5 extenções do Jupyper para o Python
+# Porém este é um exemplo para utilizar diretamente no navegador
+
+# Essa linha cria uma variável a
+a = 2
+
+# Essa linha tem o valor da variável `a` em dobro
+b = a * 2  # 4
+
+# Essa linha mostra o valor de `a` + `b`
+print(a + b)  # 6
