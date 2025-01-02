ae062c07514de93526a169331a24506fa0ca7691
lucasdeinani <125698870+lucasdeinani@users.noreply.github.com>
2025-01-02 15:17:01 -0400

    Finalizando mais uma etapa

---
+++ curso_python/aula200_Pillow-Photoshop/main.py
@@ -0,0 +1,28 @@
+# Pillow: redimensionando imagens com Python
+# Essa biblioteca é o Photoshop do Python 😂
+from PIL import Image
+
+from pathlib import Path
+
+ROOT_FOLDER = Path(__file__).parent
+ORIGINAL = ROOT_FOLDER / 'original.JPG'
+NEW_IMAGE = ROOT_FOLDER / 'new.JPG'
+
+pil_image = Image.open(ORIGINAL)
+width, height = pil_image.size
+exif = pil_image.info['exif']
+
+# width    new_width
+# height   ??
+new_width = 640
+new_height = round(height * new_width / width)
+# print(width, height)
+# print(new_width, new_height)
+
+new_image = pil_image.resize(size=(new_width, new_height))
+new_image.save(
+    NEW_IMAGE,
+    optimize=True,
+    quality=70,
+    exif=exif,
+)
